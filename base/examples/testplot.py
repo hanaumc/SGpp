805b3b9acd0876e4b05ed16de85ccc37a44eb83e
hanaumc <48556855+hanaumc@users.noreply.github.com>
2019-04-07 22:12:31 +0200

    Update testplot.py
    Auswerten weightfunction für Kreis fertig, plot fehlt

--- base/examples/testplot.py
+++ base/examples/testplot.py
@@ -1,44 +1,52 @@
-import pysgpp
+#import pysgpp
 import math
 import sys
 import numpy as np
-import matplotlib
+import matplotlib 
 import matplotlib.pyplot as plt
-import weightfunction
-from array import array
 
 
+# x wird Spaltenvektor mit einträgen von Punkt 1 in Zeile 1, Punkt 2 in Zeile 2, usw...
+
+def circle(radius, x):
+    w = radius
+    for i in range(len(x)):
+        w = w - (x[i]-0.5)**2
+    return w
+
+# Ab hier testplot    
 radius = 0.4
 dim = 2
 
-def circle(radius, x1,x2):
+def testcircle(radius, x1,x2):
     w=radius-(x1-0.5)**2-(x2-0.5)**2
     return w
 
 x1 = np.linspace(0, 1, 3)
-x2 = np.linspace(0, 1, 3)
+#x2 = np.linspace(0, 1, 3)
 
-X, Y = np.meshgrid(x1, x2)
+X = np.meshgrid(x1, x1)
 print("X={}".format(X))
-print("Y={}".format(Y))
-#print("Xausg=".format(X[[1,1]]))
-Z_testcircle = circle(0.4, X, Y)
-#Z = test(X,Y)
+Z_testcircle = testcircle(0.4, X[0], X[1])
 print("Z_testcircle={}".format(Z_testcircle))
 
-Zweightcircle = np.zeros((len(X),len(Y)))
-x = np.zeros((X.size, dim))
 
-for i in range(len(x)):
-    for j in range(dim):# hier hängt es 
-        x[i][j]= j
-print(x)
+#Zweightcircle = np.zeros((len(X),len(Y)))
+x = np.zeros((X[1].size, dim))
+#print(X[1][1][1])
 
-#for i in range(X.size):
- #   for j in range(len(x)):
-  #      print(x[i][j])
+m=0
+for k in range(len(x1)):
+  for n in range(len(x1)):
+    for j in range(dim):
+      x[m,j] = X[j][k][n]
+    m=m+1
 
-        
+#print(x)
+       
+Z_circle = circle(0.4,X)
+print("Z_circle={}".format(Z_circle))
+
 
 #for i in range(len(X)):
 #    for j in range(len(Y)):
@@ -48,6 +56,5 @@ print(x)
     
 #Z_weightcircle = weightfunction.circle(0.4,x)
 #print("Z_weightcircle={}".format(Z_weightcircle))
-#plt.contour(X, Y, Z, colors='black');
+#plt.contour(X, Y, Z_testcircle, colors='black');
 #plt.show()
-
