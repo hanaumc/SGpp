e7b3d5c9572ddf65113b33b3f747b80c70b627eb
Marc Hanauska <marc.hanauska@gmx.de>
2019-07-02 16:49:55 +0200

    Kombimethod: Gitter fertig, Monominterpol sehr spaet

--- base/examples/grid.py
+++ base/examples/grid.py
@@ -12,25 +12,27 @@ from numpy import linalg as LA, transpose, full
 from matplotlib.pyplot import axis
 
 
-level_x = 1
-level_y = 2
-dim = 2
+# level_x = 1
+# level_y = 2
+# dim = 2
 
-
-h_x = 2**(-level_x)
-h_y = 2**(-level_y)
-points_x = int(1/h_x+1)
-points_y = int(1/h_y+1)
-x = np.zeros((points_x*points_y, dim))
-print(x)
-
-for j in range(points_x):
-    for i in range(points_y):
-        print(i,j)
-
-
-
-
+def createGrid(level_x, level_y, dim):
+    h_x = 2**(-level_x)
+    h_y = 2**(-level_y)
+    points_x = int(1/h_x+1)
+    points_y = int(1/h_y+1)
+    x = np.zeros((points_x*points_y, dim))
+    ind = np.zeros((len(x),dim))
+    lvl = np.zeros((len(x),dim))
+    
+    x0=0
+    for j in range(points_y):
+        for i in range(points_x):
+            x[x0] = [i*h_x, j*h_y]
+            ind[x0] = [i,j]
+            lvl[x0] = [level_x, level_y]
+            x0=x0+1
+    return(x)
 
           
     
--- base/examples/testkombi.py
+++ base/examples/testkombi.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib 
 import matplotlib.pyplot as plt
 import weightfunction
-
+import grid
 import scipy 
 from scipy import special
 from mpl_toolkits import mplot3d
@@ -18,13 +18,28 @@ def printLine():
 
 
 
+dim = 2         # Dimension
+radius = 0.3    # Radius von Kreis
+degree = 3      # Grad von B-Splines (nur ungerade)
+level_x = 7     # Level in x Richtung    
+level_y = 7     # Level in y Richtung
 
 
+h_x = 2**(-level_x)
+h_y = 2**(-level_y)
+points_x = int(1/h_x+1)
+points_y = int(1/h_y+1)
+x = np.zeros((points_x*points_y, dim))
+ind = np.zeros((len(x),dim))
+#lvl = np.zeros((len(x),dim))
 
-dim =  2  # Dimension
-radius = 0.3  # Radius von Kreis
-degree = 1  # Grad von B-Splines (nur ungerade)
-level = 2       # Level von Sparse Grid
+x0=0
+for j in range(points_y):
+    for i in range(points_x):
+        x[x0] = [i*h_x, j*h_y]
+        ind[x0] = [i,j]
+        #lvl[x0] = [level_x, level_y]
+        x0=x0+1
 
 # Gitter fuer Kreis erzeugen und auswerten
 x0 = np.linspace(0, 1, 50)
@@ -42,31 +57,17 @@ plt.axis('equal')
 # Festlegen der Basis
 basis = pysgpp.SBsplineBase(degree)
 
-# Erzeugen von Gitter
-grid = pysgpp.Grid.createLinearBoundaryGrid(dim, degree)
-gridStorage = grid.getStorage()
-print("dimensionality:           {}".format(gridStorage.getDimension()))
-grid.getGenerator().full(level)
-print("number of grid points:    {}".format(gridStorage.getSize()))
-
-# Vektor 'x' enthaelt Koordinaten von Gitterpunkten
-# anschl. auswerten von Gewichtsfunktion des Kreises an Gitterpunkten
-x = np.zeros((gridStorage.getSize(), dim))
-index_x = np.zeros((gridStorage.getSize()))
-lvl = np.zeros((gridStorage.getSize(), dim))
-ind = np.zeros((gridStorage.getSize(), dim))
-eval_circle = np.zeros(gridStorage.getSize())
+print("dimensionality:           {}".format(dim))
+print("number of grid points:    {}".format(len(x)))
 
 
-for i in range(gridStorage.getSize()):
-    gp = gridStorage.getPoint(i)
-    lvl[i] = [gp.getLevel(0), gp.getLevel(1)]
-    ind[i] = [gp.getIndex(0), gp.getIndex(1)]
-    x[i] = [gp.getStandardCoordinate(0), gp.getStandardCoordinate(1)]
+# Auswerten von Gewichtsfunktion des Kreises an Gitterpunkten x
+index_x = np.zeros(len(x))
+eval_circle = np.zeros(len(x))
+for i in range(len(x)):
     index_x[i] = i   
     eval_circle[i] = weightfunction.circle(radius, x[i])
-
-print(x)
+    
 
 # Ueberpruefung auf innere und aeussere Punkte 
 p0 = 0
@@ -92,62 +93,62 @@ for i in range(len(eval_circle)):
 plt.scatter(I_all[:, 0], I_all[:, 1], c='mediumblue', s=50, lw=0)
 plt.scatter(J_all[:, 0], J_all[:, 1], c='crimson', s=50, lw=0)
 plt.axis('equal')
-plt.show()
+#plt.show()
 
-# Index der inneren Punkte unter Gesamtpunkten x
-index_I_all = np.zeros(len(I_all))
-for i in range(len(I_all)):
-    for j in range(len(x)):
-        if I_all[i, 0] == x[j, 0] and I_all[i, 1] == x[j, 1]:
-            index_I_all[i] = j
-
-# Index der aeusseren Punkte unter Gesamtpunkten x
-index_J_all = np.zeros(len(J_all))
-for i in range(len(J_all)):
-    for j in range(len(x)):
-        if J_all[i, 0] == x[j, 0] and J_all[i, 1] == x[j, 1]:
-            index_J_all[i] = j
-
-
-# Bestimme Gitterweite (h_x,h_y) in Abhaengigkeit vom Level
-
-h = np.zeros((len(x),dim))
-for i in range(len(h)):
-    h[i] = 2**(-lvl[i])
-
-
-# 3D Matrix
-supp_points = np.zeros((len(J_all),(degree+2)**dim, dim))
-for l in range(len(J_all)):
-    m = 0
-    for i in range(int(-(degree+1)/2),int((degree+1)/2)+1):
-        for j in range(int(-(degree+1)/2),int((degree+1)/2)+1):
-            supp_points[l,m,0] = J_all[l,0]+i*h[int(index_J_all[l]),0]
-            supp_points[l,m,1] = J_all[l,1]+j*h[int(index_J_all[l]),1]  
-            m=m+1
-#print(supp_points)
-
-
-# Bestimme relevante aeussere Punkte durch Auswerten der Gewichtsfunktion an supp_points.
-# Falls Gewichtsfunktion an einem supp_point positiv, dann ist es relevanter aeusserer Punkt
-J_relevant = np.zeros(dim)
-eval_supp_points_circle = np.zeros(supp_points.shape[1])
-for i in range(supp_points.shape[0]):
-    for j in range(supp_points.shape[1]):
-        eval_supp_points_circle[j] = weightfunction.circle(radius, supp_points[i,j])
-    if (eval_supp_points_circle > 0).any():
-        J_relevant = np.vstack((J_relevant, J_all[i]))      
-J_relevant = np.delete(J_relevant, 0, 0)
-#print(J_relevant)
-
-# Index der relevanten aeusseren Punkte unter Gesamtpunkten x
-index_J_relevant = np.zeros(len(J_relevant))
-for i in range(len(J_relevant)):
-    for j in range(len(x)):
-        if J_relevant[i, 0] == x[j, 0] and J_relevant[i, 1] == x[j, 1]:
-            index_J_relevant[i] = j
-     
-          
+# # Index der inneren Punkte unter Gesamtpunkten x
+# index_I_all = np.zeros(len(I_all))
+# for i in range(len(I_all)):
+#     for j in range(len(x)):
+#         if I_all[i, 0] == x[j, 0] and I_all[i, 1] == x[j, 1]:
+#             index_I_all[i] = j
+# 
+# # Index der aeusseren Punkte unter Gesamtpunkten x
+# index_J_all = np.zeros(len(J_all))
+# for i in range(len(J_all)):
+#     for j in range(len(x)):
+#         if J_all[i, 0] == x[j, 0] and J_all[i, 1] == x[j, 1]:
+#             index_J_all[i] = j
+# 
+# 
+# # Bestimme Gitterweite (h_x,h_y) in Abhaengigkeit vom Level
+# 
+# h = np.zeros((len(x),dim))
+# for i in range(len(h)):
+#     h[i] = 2**(-lvl[i])
+# 
+# 
+# # 3D Matrix
+# supp_points = np.zeros((len(J_all),(degree+2)**dim, dim))
+# for l in range(len(J_all)):
+#     m = 0
+#     for i in range(int(-(degree+1)/2),int((degree+1)/2)+1):
+#         for j in range(int(-(degree+1)/2),int((degree+1)/2)+1):
+#             supp_points[l,m,0] = J_all[l,0]+i*h[int(index_J_all[l]),0]
+#             supp_points[l,m,1] = J_all[l,1]+j*h[int(index_J_all[l]),1]  
+#             m=m+1
+# #print(supp_points)
+# 
+# 
+# # Bestimme relevante aeussere Punkte durch Auswerten der Gewichtsfunktion an supp_points.
+# # Falls Gewichtsfunktion an einem supp_point positiv, dann ist es relevanter aeusserer Punkt
+# J_relevant = np.zeros(dim)
+# eval_supp_points_circle = np.zeros(supp_points.shape[1])
+# for i in range(supp_points.shape[0]):
+#     for j in range(supp_points.shape[1]):
+#         eval_supp_points_circle[j] = weightfunction.circle(radius, supp_points[i,j])
+#     if (eval_supp_points_circle > 0).any():
+#         J_relevant = np.vstack((J_relevant, J_all[i]))      
+# J_relevant = np.delete(J_relevant, 0, 0)
+# #print(J_relevant)
+# 
+# # Index der relevanten aeusseren Punkte unter Gesamtpunkten x
+# index_J_relevant = np.zeros(len(J_relevant))
+# for i in range(len(J_relevant)):
+#     for j in range(len(x)):
+#         if J_relevant[i, 0] == x[j, 0] and J_relevant[i, 1] == x[j, 1]:
+#             index_J_relevant[i] = j
+#      
+#           
 
 # # Plotte relevante Punkte
 # plt.scatter(I_all[:, 0], I_all[:, 1], c='mediumblue', s=50, lw=0)
@@ -161,75 +162,77 @@ for i in range(len(J_relevant)):
 # Anzahl nearest neighbors
 n_neighbors = (degree+1)**2
 
+#print(x)
 # Monome definieren und an allen Gitterpunkten auswerten
 size_monomials = n_neighbors
-eval_monomials = np.zeros((size_monomials, gridStorage.getSize()))
+eval_monomials = np.zeros((size_monomials, len(x)))
+#print(eval_monomials.shape)
 k = 0
 for j in range(degree + 1):
     for i in range (degree + 1):
-        if i + j <= degree:
-            eval_monomials[k] = (pow(x[:, 0], i) * pow(x[:, 1], j))
-            k = k + 1    
+        eval_monomials[k] = (pow(x[:, 0], i) * pow(x[:, 1], j))
+        k = k + 1   
 eval_monomials = np.transpose(eval_monomials)
 #print(eval_monomials)
-
+ 
 # Definiere Matrix A fuer Interpolation der coeffs
-A = np.zeros((gridStorage.getSize(), gridStorage.getSize()))
-for i in range(gridStorage.getSize()):
-    for j in range(gridStorage.getSize()):
-        A[i, j] = basis.eval(int(lvl[j, 0]), int(ind[j, 0]), x[i, 0]) * basis.eval(int(lvl[j, 1]), int(ind[j, 1]), x[i, 1])   
-#print(A)
-
-# x_str = np.array_repr(A).replace('\n', '')
-# print(x_str)
-
-# Loese LGS und erhalte coeffs
-coeffs = np.linalg.solve(A, eval_monomials)
-#print(coeffs)
-
-# # Beliebige Punkte im Gebiet
-# punkte = np.zeros((20, 2))
-# counter = 0 
-# while counter < 20:
-#     z = np.random.rand(1, 2)
-#     if weightfunction.circle(radius, z[0]) > 0:
-#         punkte[counter] = z[0]
-#         counter = counter + 1
-# for j in range(punkte.shape[0]):
-#     summe = 0
-#     for i in range(len(x)):
-#         summe = summe + coeffs[i,0]*basis.eval(int(lvl[i,0]), int(ind[i,0]), punkte[j,0])*basis.eval(int(lvl[i,1]), int(ind[i,1]), punkte[j,1])
-#     print(punkte[j])
-#     print(summe)
-#     fehler = summe - 1                         #coeffs[i,0], Monom 1
-#     #    fehler = summe - punkte[j,0]                #coeffs[i,1], Monom x
-#     #    fehler = summe - punkte[j,1]               #coeffs[i,2], Monom y
-#     #    fehler = summe-(punkte[j,0]*punkte[j,1])   #coeffs[i,3], Monom x*y
-#     print(fehler)
-#     printLine()
-
-
-# # Beliebige Punkte im Einheitsquadrat
-# punkte = np.random.rand(20,2)
-# for j in range(punkte.shape[0]):
-#     summe = 0
-#     for i in range(len(x)):
-#         summe = summe + coeffs[i,0]*basis.eval(int(lvl[i,0]), int(ind[i,0]), punkte[j,0])*basis.eval(int(lvl[i,1]), int(ind[i,1]), punkte[j,1])
-#     print(punkte[j])
-#     print(summe)
-#     fehler = summe - 1                         #coeffs[i,0], Monom 1
-# #    fehler = summe - punkte[j,0]                #coeffs[i,1], Monom x
-# #    fehler = summe - punkte[j,1]               #coeffs[i,2], Monom y
-# #    fehler = summe-(punkte[j,0]*punkte[j,1])   #coeffs[i,3], Monom x*y
-#     print(fehler)
-#     printLine()
-    
-        
-# Test ob Loesen des LGS erfolgreich war
-error = eval_monomials - np.matmul(A, coeffs)
-error = LA.norm(error)
-if error > pow(10, -14):
-    print('failed. error > 10e-14')
-
+A = np.zeros((len(x), len(x)))
+for i in range(len(x)):
+    for j in range(len(x)):
+        A[i, j] = basis.eval(int(level_x), int(ind[j, 0]), x[i, 0]) * basis.eval(int(level_y), int(ind[j, 1]), x[i, 1])   
+#print(A.shape)
+#  
+# # x_str = np.array_repr(A).replace('\n', '')
+# # print(x_str)
+#  
+# # Loese LGS und erhalte coeffs
+# coeffs = np.linalg.solve(A, eval_monomials)
+# #print(coeffs)
+#  
+# # # Beliebige Punkte im Gebiet
+# # punkte = np.zeros((20, 2))
+# # counter = 0 
+# # while counter < 20:
+# #     z = np.random.rand(1, 2)
+# #     if weightfunction.circle(radius, z[0]) > 0:
+# #         punkte[counter] = z[0]
+# #         counter = counter + 1
+# # for j in range(punkte.shape[0]):
+# #     summe = 0
+# #     for i in range(len(x)):
+# #         summe = summe + coeffs[i,0]*basis.eval(int(lvl[i,0]), int(ind[i,0]), punkte[j,0])*basis.eval(int(lvl[i,1]), int(ind[i,1]), punkte[j,1])
+# #     #print(punkte[j])
+# #     #print(summe)
+# #     #fehler = summe - 1                         #coeffs[i,0], Monom 1
+# #     fehler = summe - punkte[j,0]                #coeffs[i,1], Monom x
+# #     #    fehler = summe - punkte[j,1]               #coeffs[i,2], Monom y
+# #     #    fehler = summe-(punkte[j,0]*punkte[j,1])   #coeffs[i,3], Monom x*y
+# #     #print(fehler)
+# #     #printLine()
+#  
+#  
+# # # Beliebige Punkte im Einheitsquadrat
+# # punkte = np.random.rand(20,2)
+# # for j in range(punkte.shape[0]):
+# #     summe = 0
+# #     for i in range(len(x)):
+# #         summe = summe + coeffs[i,0]*basis.eval(int(lvl[i,0]), int(ind[i,0]), punkte[j,0])*basis.eval(int(lvl[i,1]), int(ind[i,1]), punkte[j,1])
+# #     print(punkte[j])
+# #     print(summe)
+# #     fehler = summe - 1                         #coeffs[i,0], Monom 1
+# # #    fehler = summe - punkte[j,0]                #coeffs[i,1], Monom x
+# # #    fehler = summe - punkte[j,1]               #coeffs[i,2], Monom y
+# # #    fehler = summe-(punkte[j,0]*punkte[j,1])   #coeffs[i,3], Monom x*y
+# #     print(fehler)
+# #     printLine()
+#      
+#          
+# # Test ob Loesen des LGS erfolgreich war
+# error = eval_monomials - np.matmul(A, coeffs)
+# error = LA.norm(error)
+# print(error)
+# if error > pow(10, -14):
+#     print('failed. error > 10e-14')
+#  
 
 
