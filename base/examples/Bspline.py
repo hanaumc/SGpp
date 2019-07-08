9d20671935b4ef7befa6b967c8e0bccab55a3373
Marc Hanauska <marc.hanauska@gmx.de>
2019-07-08 15:16:18 +0200

    NaK Bedingung einbauen, NN implementieren und testen

--- base/examples/Bspline.py
+++ base/examples/Bspline.py
@@ -18,13 +18,13 @@ def printLine():
 
 def evalBspline(n, k, xi, x):
     if n == 0:
-        return 1.0 if xi[k] <= x < xi[k+1] else 0.0
+        return 1 if xi[k] <= x < xi[k+1] else 0
     if xi[k+n] == xi[k]:
-        c1 = 0.0
+        c1 = 0
     else:
         c1 = (x-xi[k])/(xi[k+n]-xi[k]) * evalBspline(n-1, k, xi, x)
     if xi[k+n+1] == xi[k+1]:
-        c2 = 0.0
+        c2 = 0
     else:
         c2 = (1-(x-xi[k+1])/(xi[k+n+1]-xi[k+1])) * evalBspline(n-1, k+1, xi, x)
     return c1 + c2
--- base/examples/testkombi.py
+++ base/examples/testkombi.py
@@ -21,10 +21,10 @@ def printLine():
 # print(a)
 
 dim = 2         # Dimension
-radius = 0.2    # Radius von Kreis
+radius = 0.40    # Radius von Kreis
 degree = 3      # Grad von B-Splines (nur ungerade)
-level_x = 5     # Level in x Richtung    
-level_y = 5     # Level in y Richtung
+level_x = 3     # Level in x Richtung    
+level_y = 3     # Level in y Richtung
 
 # Gitter fuer Kreis erzeugen und auswerten
 x0 = np.linspace(0, 1, 50)
@@ -103,7 +103,7 @@ J_relevant = np.delete(J_relevant, 0, 0)
 plt.scatter(grid[0], grid[1], c='crimson', s=50, lw=0)
 plt.scatter(I_all[:,0], I_all[:,1], c='mediumblue', s=50, lw=0)
 plt.scatter(J_relevant[:, 0], J_relevant[:, 1], c='goldenrod', s=50, lw=0)
-#plt.show()
+plt.show()
 
 # Monome definieren und an allen Gitterpunkten auswerten
 size_monomials = (degree+1)**2
@@ -135,6 +135,7 @@ for l in range(len(gp)):
 # Loese LGS und erhalte coeffs
 coeffs = np.linalg.solve(A, eval_monomials)
 #print(coeffs) 
+#print(coeffs.shape)
 
 # Test ob Loesen des LGS erfolgreich war
 error = eval_monomials - np.matmul(A, coeffs)
@@ -142,7 +143,7 @@ error = LA.norm(error)
 #print(error)
 if error > pow(10, -14):
     print('failed. error > 10e-14')
-printLine()
+
 
 
 # Beliebige Punkte im Gebiet
@@ -155,7 +156,7 @@ while counter < anzahl:
         punkte[counter] = z[0]
         counter = counter + 1
 
-punkt = [0.45,0.6]
+# punkt = [0.45,0.6]
 # for i in range(len(index_Bspline_x)):
 #     print(Bspline.evalBspline(degree, i, xi, punkt[0]))
 # printLine()
@@ -172,36 +173,16 @@ for k in range(len(punkte)):
     c = 0
     for i in range(len(index_Bspline_x)):
         for j in range(len(index_Bspline_y)):
-            summe = summe + coeffs[c,0] * Bspline.evalBspline(degree, i, xi, punkte[k,0]) * Bspline.evalBspline(degree, j, yi, punkte[k,1])
+            summe = summe + coeffs[c,3] * Bspline.evalBspline(degree, i, xi, punkte[k,0]) * Bspline.evalBspline(degree, j, yi, punkte[k,1])
             c=c+1
-    fehler = summe - 1                          #coeffs[i,0], Monom 1                    
-    #fehler = summe - punkte[j,0]               #coeffs[i,1], Monom x
-    #fehler = summe - punkte[j,1]               #coeffs[i,2], Monom y
-    #fehler = summe-(punkte[j,0]*punkte[j,1])   #coeffs[i,3], Monom x*y
-#     print(fehler)
-#     printLine()
+    #fehler = summe - 1                          #coeffs[c,0], Monom 1                    
+    #fehler = summe - punkte[k,0]               #coeffs[c,1], Monom x
+    #fehler = summe - punkte[k,1]               #coeffs[c,2], Monom y
+    fehler = summe-(punkte[k,0]*punkte[k,1])   #coeffs[c,3], Monom x*y
     L2fehler = L2fehler + fehler**2
 L2fehler = np.sqrt(L2fehler) 
 print(L2fehler)
-
-        
-        
-        
-# for j in range(len(punkte)):
-#     summe = 0
-# 
-#     for l in range(len(index_Bspline_x)):
-#         for m in range(len(index_Bspline_y)):
-#             summe = summe + coeffs[i] * Bspline.evalBspline(degree, l, xi, punkte[j,0]) * Bspline.evalBspline(degree, m, yi, punkte[j,1])
-#     #summe = summe + coeffs[i,1]*basis.eval(int(level_x), int(ind[i,0]), punkte[j,0])*basis.eval(int(level_y), int(ind[i,1]), punkte[j,1])
-# #print(punkte[j])
-# print(summe)
-# #fehler = summe - 1                         #coeffs[i,0], Monom 1
-# fehler = summe - punkte[j,0]                #coeffs[i,1], Monom x
-# #    fehler = summe - punkte[j,1]               #coeffs[i,2], Monom y
-# #    fehler = summe-(punkte[j,0]*punkte[j,1])   #coeffs[i,3], Monom x*y
-# print(fehler)
-# printLine()
+printLine()
   
   
 # # Beliebige Punkte im Einheitsquadrat
@@ -229,17 +210,55 @@ print(L2fehler)
 # if error > pow(10, -14):
 #     print('failed. error > 10e-14')
   
-  
 
+# # Definiere Koeffizientenmatrix der aeusseren relevanten Punkte
+# coeffs_J_relevant = np.zeros((len(J_relevant), size_monomials))
+# for j in range(size_monomials):
+#     for i in range(len(J_relevant)):
+#         coeffs_J_relevant[i,j] = coeffs[int(index_J_relevant[i]),j]
+# coeffs_J_relevant = transpose(coeffs_J_relevant)
+# #print(coeffs_J_relevant)
 # 
 # 
-# # Auswerten von Gewichtsfunktion des Kreises an Gitterpunkten x
-# index_x = np.zeros(len(x))
-# eval_circle = np.zeros(len(x))
-# for i in range(len(x)):
-#     index_x[i] = i   
-#     eval_circle[i] = weightfunction.circle(radius, x[i])
+# # Definiere Koeffizientenmatrix der inneren Punkte zu aeusseren Punkten 
+# coeffs_inner_NN = np.zeros((len(J_relevant),size_monomials, n_neighbors))
+# for k in range(len(J_relevant)):
+#     for j in range(n_neighbors):
+#         for i in range(size_monomials):
+#             coeffs_inner_NN[k,i,j] = coeffs[int(index_NN[j,k]),i]
+# #print(coeffs_inner_NN)
+# #print(coeffs_inner_NN.shape)
+# # inv = np.linalg.inv(coeffs_inner_NN[0])
+# # print(inv)
+# # det = np.linalg.det(coeffs_inner_NN)
+# # print(det)
 # 
+# 
+# # Berechne Extensioncoefficient
+# extension_coeffs = np.zeros((len(J_relevant),n_neighbors))
+# for i in range(coeffs_inner_NN.shape[0]):
+#     solution=np.linalg.solve(coeffs_inner_NN[i], coeffs_J_relevant[:,i])
+#     extension_coeffs[i] = solution[0]
+# extension_coeffs = transpose(extension_coeffs)
+# #print(extension_coeffs)
+# 
+# # Definiere J(i) 
+# J_i = np.zeros((index_NN.shape[1], len(I_all)))  # x
+# for i in range(len(I_all)):#index_x
+#     for j in range((index_NN.shape[1])):
+#         for k in range((index_NN.shape[0])):
+#             if index_x[i] == index_NN[k, j]:
+#                 # print(i,index_J_relevant[j])
+#                 J_i[j, i] = index_J_relevant[j]
+# #print(J_i)  
+
+
+
+
+
+
+
+ 
 # # # Index der inneren Punkte unter Gesamtpunkten x
 # # index_I_all = np.zeros(len(I_all))
 # # for i in range(len(I_all)):
@@ -255,14 +274,6 @@ print(L2fehler)
 # #             index_J_all[i] = j
 # # 
 # # 
-# # # Bestimme Gitterweite (h_x,h_y) in Abhaengigkeit vom Level
-# # 
-# # h = np.zeros((len(x),dim))
-# # for i in range(len(h)):
-# #     h[i] = 2**(-lvl[i])
-# # 
-# # 
-# # 
 # # # Index der relevanten aeusseren Punkte unter Gesamtpunkten x
 # # index_J_relevant = np.zeros(len(J_relevant))
 # # for i in range(len(J_relevant)):
@@ -276,79 +287,8 @@ print(L2fehler)
 # # Anzahl nearest neighbors
 # n_neighbors = (degree+1)**2
 # 
-# #print(x)
-# # Monome definieren und an allen Gitterpunkten auswerten
-# size_monomials = n_neighbors
-# eval_monomials = np.zeros((size_monomials, len(x)))
-# #print(eval_monomials.shape)
-# k = 0
-# for j in range(degree + 1):
-#     for i in range (degree + 1):
-#         eval_monomials[k] = (pow(x[:, 0], i) * pow(x[:, 1], j))
-#         k = k + 1   
-# eval_monomials = np.transpose(eval_monomials)
-# #print(eval_monomials)
 #  
-# # Definiere Matrix A fuer Interpolation der coeffs
-# A = np.zeros((len(x), len(x)))
-# for i in range(len(x)):
-#     for j in range(len(x)):
-#         A[i, j] = basis.eval(int(level_x), int(ind[j, 0]), x[i, 0]) * basis.eval(int(level_y), int(ind[j, 1]), x[i, 1])   
-# #print(A.shape)
 # #  
 # # # x_str = np.array_repr(A).replace('\n', '')
 # # # print(x_str)
-# #  
-# # Loese LGS und erhalte coeffs
-# coeffs = np.linalg.solve(A, eval_monomials)
-# #print(coeffs)
-# #  
-# # Beliebige Punkte im Gebiet
-# 
-# anzahl = 100
-# punkte = np.zeros((anzahl, 2))
-# counter = 0 
-# while counter < anzahl:
-#     z = np.random.rand(1, 2)
-#     if weightfunction.circle(radius, z[0]) > 0:
-#         punkte[counter] = z[0]
-#         counter = counter + 1
-# for j in range(punkte.shape[0]):
-#     summe = 0
-#     for i in range(len(x)):
-#         summe = summe + coeffs[i,1]*basis.eval(int(level_x), int(ind[i,0]), punkte[j,0])*basis.eval(int(level_y), int(ind[i,1]), punkte[j,1])
-#     #print(punkte[j])
-#     #print(summe)
-#     #fehler = summe - 1                         #coeffs[i,0], Monom 1
-#     fehler = summe - punkte[j,0]                #coeffs[i,1], Monom x
-#     #    fehler = summe - punkte[j,1]               #coeffs[i,2], Monom y
-#     #    fehler = summe-(punkte[j,0]*punkte[j,1])   #coeffs[i,3], Monom x*y
-# #     print(fehler)
-# #     printLine()
-# #  
-# #  
-# # # # Beliebige Punkte im Einheitsquadrat
-# # # punkte = np.random.rand(20,2)
-# # # for j in range(punkte.shape[0]):
-# # #     summe = 0
-# # #     for i in range(len(x)):
-# # #         summe = summe + coeffs[i,0]*basis.eval(int(lvl[i,0]), int(ind[i,0]), punkte[j,0])*basis.eval(int(lvl[i,1]), int(ind[i,1]), punkte[j,1])
-# # #     print(punkte[j])
-# # #     print(summe)
-# # #     fehler = summe - 1                         #coeffs[i,0], Monom 1
-# # # #    fehler = summe - punkte[j,0]                #coeffs[i,1], Monom x
-# # # #    fehler = summe - punkte[j,1]               #coeffs[i,2], Monom y
-# # # #    fehler = summe-(punkte[j,0]*punkte[j,1])   #coeffs[i,3], Monom x*y
-# # #     print(fehler)
-# # #     printLine()
-# #      
-# #          
-# # # Test ob Loesen des LGS erfolgreich war
-# # error = eval_monomials - np.matmul(A, coeffs)
-# # error = LA.norm(error)
-# # print(error)
-# # if error > pow(10, -14):
-# #     print('failed. error > 10e-14')
-# #  
-
 
